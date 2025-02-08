b00ee33d6d93238d4e7b9c1012cfd3b79ec75d46
VILLAIN <iamrkay420@gmail.com>
2025-02-08 10:23:51 +0530

    Update welcome.py

--- SONALI/plugins/tools/welcome.py
+++ SONALI/plugins/tools/welcome.py
@@ -78,13 +78,13 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("SACHIN_MUSIC/assets/wel2.png")
+    background = Image.open("SONALI/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
     pfp = pfp.resize((500, 500))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('SACHIN_MUSIC/assets/font.ttf', size=60)
-    welcome_font = ImageFont.truetype('SACHIN_MUSIC/assets/font.ttf', size=60)
+    font = ImageFont.truetype('SONALI/assets/font.ttf', size=60)
+    welcome_font = ImageFont.truetype('SONALI/assets/font.ttf', size=60)
     
  #   draw.text((630, 230), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
    # draw.text((630, 300), f'NAME: {user}', fill=(255, 255, 255), font=font)
@@ -146,7 +146,7 @@ async def greet_new_member(_, member: ChatMemberUpdated):
                 user.photo.big_file_id, file_name=f"pp{user.id}.png"
             )
         except AttributeError:
-            pic = "SACHIN_MUSIC/assets/upic.png"
+            pic = "SONALI/assets/upic.png"
         if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
             try:
                 await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
