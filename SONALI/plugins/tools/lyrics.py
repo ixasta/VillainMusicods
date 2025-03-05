05e0d84f826d89e3221980e6b306db26aebd7dad
VILLAIN <iamrkay420@gmail.com>
2025-03-05 08:55:32 +0530

    Update lyrics.py

--- SONALI/plugins/tools/lyrics.py
+++ SONALI/plugins/tools/lyrics.py
@@ -1,12 +1,4 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 """
 import random
 import re
@@ -18,8 +10,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from VIPMUSIC import app
-from VIPMUSIC.utils.decorators.language import language
+from SONALI import app
+from SONALI.utils.decorators.language import language
 
 ###Commands
 LYRICS_COMMAND = get_command("LYRICS_COMMAND")
